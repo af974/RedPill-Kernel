bd050530072697bf7d2caeb315276996cd1accaf
Andrea Ferrario <ferrarioand@gmail.com>
2012-02-19 15:14:17 +0100

    first commit

---
+++ build.sh
@@ -0,0 +1,8 @@
+#Build script for Galaxy Tab
+#Written Frank aka AlterBridge86
+
+cd drivers/misc/samsung_modemctl
+make -j2
+cd ../../..
+
+make -j2
---
+++ include/generated/compile.h
@@ -0,0 +1,9 @@
+/* This file is auto generated, version 52 */
+/*  PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#52 PREEMPT Sun Feb 19 14:49:50 CET 2012"
+#define LINUX_COMPILE_TIME "14:49:50"
+#define LINUX_COMPILE_BY "RP_v2.0.0"
+#define LINUX_COMPILE_HOST ""
+#define LINUX_COMPILE_DOMAIN "(none)"
+#define LINUX_COMPILER "gcc version 4.5.2 (Sourcery G++ Lite 2011.03-41) "
